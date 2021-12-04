38c10cd4d0c43582afad2cf6cc0292f6477090b5
django <sashchystyh@yandex.ru>
2021-12-04 18:44:37 +0300

    feat: parser which implemented by class

---
+++ task_1/parser.py
@@ -0,0 +1,91 @@
+import json
+import os
+
+from dict2xml import dict2xml
+
+
+class RoomData:
+    """The class takes the paths to the json files with the list of students and rooms and the output format type.
+    Supported output file types: "json", "xml"."""
+
+    __slots__ = ("path_to_rooms", "path_to_students", "output_format")
+    __supported_output_formats: tuple = ("json", "xml")
+
+    def __call__(self, *args, **kwargs) -> str:
+        if self.output_format == "xml":
+            return self.get_result_xml()
+        elif self.output_format == "json":
+            return self.get_result_json()
+
+    def __init__(self, path_to_rooms: str, path_to_students: str, output_format: str) -> None:
+        if os.path.isfile(path_to_rooms):
+            self.path_to_rooms: str = path_to_rooms
+        else:
+            raise FileNotFoundError(f"'{path_to_rooms}' is not a file or does not exist.")
+
+        if os.path.isfile(path_to_students):
+            self.path_to_students: str = path_to_students
+        else:
+            raise FileNotFoundError(f"'{path_to_students}' is not a file or does not exist.")
+
+        if output_format in self.__supported_output_formats:
+            self.output_format = output_format
+        else:
+            raise TypeError(
+                f"Unknown output format '{output_format}'. Use {' or '.join(self.__supported_output_formats)}."
+            )
+
+    def get_data_dicts(self) -> tuple:
+        with open(self.path_to_rooms) as json_rooms:
+            rooms = json.load(json_rooms)
+        with open(self.path_to_students) as json_students:
+            students = json.load(json_students)
+        return rooms, students
+
+    def compare_input_data(self) -> list:
+        is_xml_output = self.output_format == "xml"
+        rooms, students = self.get_data_dicts()
+        list(map(lambda room: room.update({"students": {}}), rooms)) if is_xml_output else None
+        list(
+            map(
+                lambda room: (room.get("students") if is_xml_output else room).update(
+                    {
+                        f"student{'s' if not is_xml_output else ''}": [
+                            {"id": student.get("id"), "name": student.get("name")}
+                            for student in students
+                            if student.get("room") == room.get("id")
+                        ]
+                    }
+                ),
+                rooms,
+            )
+        )
+        return rooms
+
+    def get_result_json(self) -> str:
+        json_data: str = json.dumps(self.compare_input_data())
+        self.write_output_file(json_data)
+        return json_data
+
+    def get_result_xml(self) -> str:
+        xml_data: str = dict2xml(self.compare_input_data(), wrap="room")
+        xml_data = "<rooms>\n" + xml_data + "\n</rooms>"
+        self.write_output_file(xml_data)
+        return xml_data
+
+    def write_output_file(self, data: str) -> None:
+        with open(f"output_data.{self.output_format}", "w") as output:
+            output.write(data)
+
+
+def main():
+    print(RoomData.__doc__)
+    path_to_rooms = input("Please, enter the path to json file with rooms data: ")
+    path_to_students = input("Please, enter the path to json file with students data: ")
+    output_format = input("Please, enter the output format: ")
+    RoomData(path_to_rooms, path_to_students, output_format)()
+    print("Well done!")
+
+
+if __name__ == "__main__":
+    main()
